9f7f3bb96d51ba7f3e6100c3b7fc3b5c047afe74
Emzzy241 <emzzyoluwole@gmail.com>
2026-05-30 18:55:57 +0100

    update email routes to test get emails

--- src/routes/email.routes.js
+++ src/routes/email.routes.js
@@ -5,7 +5,7 @@ const authMiddleware = require("../middleware/auth.middleware");
 const { getEmails } = require("../controllers/email.controller");
 
 // Removing jwt for now to test gmail integrations
-router.get("/", getEmails);
+router.get("/", getEmails); // currently removed jwt just for the goal of testing things out 
 // router.get("/", authMiddleware, getEmails);
 
 module.exports = router;
