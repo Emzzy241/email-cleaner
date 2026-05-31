4b4a793ab69970fb87fae1446afea803f39ba925
Emzzy241 <emzzyoluwole@gmail.com>
2026-05-31 19:23:25 +0100

    add POST /cleanup route

--- src/routes/email.routes.js
+++ src/routes/email.routes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const authMiddleware = require("../middleware/auth.middleware");
-const { getEmails } = require("../controllers/email.controller");
+const { getEmails, cleanupEmails } = require("../controllers/email.controller");
 
 // Removing jwt for now to test gmail integrations
 router.get("/", getEmails); // currently removed jwt just for the goal of testing things out 
 // router.get("/", authMiddleware, getEmails);
+
+router.post("/cleanup", cleanupEmails);
 
 module.exports = router;
