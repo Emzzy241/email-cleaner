a4dc1f2c9ee4bcc151d5085663fcc11eb92d93bd
Emzzy241 <emzzyoluwole@gmail.com>
2026-06-06 06:16:11 +0100

    add routes: /cleanup-jobs/:id, /clean-all-jobs, /cleanup/old-emails

--- src/routes/email.routes.js
+++ src/routes/email.routes.js
@@ -2,12 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 const authMiddleware = require("../middleware/auth.middleware");
-const { getEmails, cleanupEmails } = require("../controllers/email.controller");
+const { getEmails, cleanupEmails, getCleanupStatus, getAllCleanupStatus, cleanupOldEmails } = require("../controllers/email.controller");
 
 // Removing jwt for now to test gmail integrations
 router.get("/", getEmails); // currently removed jwt just for the goal of testing things out 
 // router.get("/", authMiddleware, getEmails);
 
+router.get("/cleanup-jobs/:id", getCleanupStatus);
+router.get("/cleanup-all-jobs", getAllCleanupStatus); // status for all jobs currently being cleaned up.
+
 router.post("/cleanup", cleanupEmails);
+router.post("/cleanup/old-emails", cleanupOldEmails);
 
 module.exports = router;
